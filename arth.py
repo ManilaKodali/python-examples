01422365ce92a92dcf9a3d496c138af5e00a64bd
ManilaKodali <kdlmanila36@gmail.com>
2019-01-19 07:07:31 +0200

    Arithmetic operations

---
+++ arth.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+print("Enter which operation would you like to perform?")
+ch = input("Enter any of these char for specific operation +,-,*,/:")
+result = 0
+if ch == '+':
+    result = num1 + num2
+elif ch == '-':
+    result = num1 - num2
+elif ch == '*':
+    result = num1 * num2
+elif ch == '/':
+    result = num1 / num2
+else:
+    print(num1 , ch , num2, ":",result)
+
+
